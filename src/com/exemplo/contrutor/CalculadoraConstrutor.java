b23d762684e1d622dbc05e22504839b6358e0051
eduardotrandafilov <eduardo.trandafilov@gmail.com>
2020-01-18 12:47:59 -0300

    Projeto de estudo para metodos-construtores

---
+++ src/com/exemplo/contrutor/CalculadoraConstrutor.java
@@ -0,0 +1,23 @@
+package com.exemplo.contrutor;
+
+public class CalculadoraConstrutor {
+
+	private int numero1, numero2;
+	
+	public CalculadoraConstrutor() {}
+		
+	public CalculadoraConstrutor(int valor1,int valor2) {
+		numero1=valor1;
+		numero2=valor2;
+		
+		System.out.println(numero1);
+		System.out.println(numero2);
+
+
+	}
+	
+	
+	
+	
+	
+}
---
+++ src/com/exemplo/teste/Main.java
@@ -0,0 +1,16 @@
+package com.exemplo.teste;
+
+import com.exemplo.contrutor.CalculadoraConstrutor;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		CalculadoraConstrutor calcula;
+		
+		calcula = new CalculadoraConstrutor(10,20);
+		
+	}
+
+}
